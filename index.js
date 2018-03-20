8f5c198de8c0e7d4cb29b893416f6547530a7225
Jeú Guimarães Conrado Junior <jeucasulo@hotmail.com>
2018-03-20 01:27:43 -0300

    String Generate for Routes, Controller, Model e Request

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 $(document).ready(function(){
+	/*----------  Index  ----------*/
 	Index.GettingJsonTableData();
 	Index.SetingSelects();
 	Index.CreatingNewJsonFile();
 	$("#updateJsonTable").click(Index.UpdatingAndSavingCode);
 	$("#addNewCOlumn").click(Index.AddNewColumn);
 	$(".deleteColumn").on('click', "deleteColumn",Index.DeleteColumn);
+
+	/*----------  Controller  ----------*/
+	$("#generateController").click(Controller.GenerateControllerString);
+
+	/*----------  Routes  ----------*/
+	$("#generateRoutes").click(Routes.GenerateRoutesString);
+
+	/*----------  Model  ----------*/
+	$("#generateModel").click(Model.GenerateModelString);
+
+	/*----------  Request  ----------*/
+	$("#generateRequest").click(Request.GenerateRequestString);
+
+
+
+
 	
-	// $("#updateJsonTable").click(function(e){
-	//   // e.preventDefault();
-	// });
-
-
 
 });
+
 var Globals = {
 	tableName :"table1.json",
 	totalColumns : $("#totalColumns").html(),
@@ -217,23 +230,258 @@ var Index ={
 	}
 
 };
+var Routes = {
+	/*----------  Subsection comment block  ----------*/
+	
+	GenerateRoutesString:function(){
+
+		let tableName = $("#tableName").val();
+		let tableSingular = $("#tableSingular").val();
+		let tablePlural = $("#tablePlural").val();
+
+		let routes = "/*---------- "+tableName+" ----------*/\n";
+		routes += "Route::get('/crud/"+tableSingular+"', ['uses' => '"+tableName+"Controller@index', 'as' => 'cruds."+tableSingular+".index']);\n";
+		routes += "Route::get('/crud/"+tableSingular+"/show/{id}', ['uses' => '"+tableName+"Controller@show', 'as' => 'cruds."+tableSingular+".show']);\n";
+		routes += "Route::get('/crud/"+tableSingular+"/create', ['uses' => '"+tableName+"Controller@create', 'as' => 'cruds."+tableSingular+".create']);\n";
+		routes += "Route::post('/crud/"+tableSingular+"/store', ['uses' => '"+tableName+"Controller@store', 'as' => 'cruds."+tableSingular+".store']);\n";
+		routes += "Route::get('/crud/"+tableSingular+"/edit/{id}', ['uses' => '"+tableName+"Controller@edit', 'as' => 'cruds."+tableSingular+".edit']);\n";
+		routes += "Route::put('/crud/"+tableSingular+"/update/{id}', ['uses' => '"+tableName+"Controller@update', 'as' => 'cruds."+tableSingular+".update']);\n";
+		routes += "Route::get('/crud/"+tableSingular+"/destroy/{id}', ['uses' => '"+tableName+"Controller@destroy', 'as' => 'cruds."+tableSingular+".destroy']);\n";
+		routes += "/*---------- "+tableName+" ----------*/\n";
+
+		$("#routes_string_output").html(routes);
+	},
+};
+var Controller = {
+	GenerateControllerString:function(){
+		let col_id = "id";
+		let tableName = $("#tableName").val();
+		let tableSingular = $("#tableSingular").val();
+		let tablePlural = $("#tablePlural").val();
+		let totalColumns = $("#totalColumns").html();
+
+		let controllerOutput = "";
+		controllerOutput += "|| double bar"; 
+		controllerOutput += "<\?php \n|namespace App\\Http\\Controllers; \n|use Illuminate\\Http\\Request;\n|use App\\Http\\Requests;  "; 
+		controllerOutput += "\r\n|\n|class "+tableName+"Controller extends Controller\n|{";
+		
+		//index
+		controllerOutput += "\n|\t/** \n|\t * Display a listing of the resource. \n|\t * \n|\t * @return \\Illuminate\\Http\\Response \n |\t */ \n|\tpublic function index() \n|\t{";
+		controllerOutput += "\n|\t\t$"+tablePlural+" = \\App\\Models\\"+tableName+"::all();";
+		controllerOutput += "\n|\t\treturn view('cruds."+tableSingular+".index', compact('"+tablePlural+"'));"; 
+		controllerOutput += "\n|\t}";
+		
+		//create
+		controllerOutput += "\n |\n |\t/** \n |\t * Show the form for creating a new resource. \n |\t * \n |\t * @return \\Illuminate\\Http\\Response\n |\t */\n |\tpublic function create()\n |\t {";
+		controllerOutput += "\n |\t\tif(999==999){ // input your acl or condition";
+		controllerOutput += "\n |\t\t\t//return redirect()->route('cruds."+tablePlural+".create');";
+		controllerOutput += "\n |\t\t\treturn view('cruds."+tableSingular+".create');";
+		controllerOutput += "\n |\t\t}else{\n |\t\t\treturn redirect()->route('cruds."+tablePlural+".index');\n |\t\t}";
+		controllerOutput += "\n |\t}";
+
+		//store
+		controllerOutput += "\n |\n |\t/** \n |\t * Store a newly created resource in storage. \n |\t * \n |\t * @param  \\Illuminate\\Http\\Request  $request \n |\t * @return \\Illuminate\\Http\\Response\n |\t */\n |\tpublic function store(\\App\\Http\\Requests\\"+tableName+"Request $request)\n |\t{//Request $request";
+		controllerOutput += "\n |\t\tif(999==999){ // input your acl or condition";
+		controllerOutput += "\n |\t\t\t\\App\\Models\\"+tableName+"::create($request->all());";
+		controllerOutput += "\n |\t\t\t\//$last_id = \\App\\"+tableName+"::limit(1)->orderBy('"+tableSingular+"_id','desc')->value('"+tableSingular+"_id');";
+		controllerOutput += "\n |\t\t\t//$"+tableSingular+" = \\App\\"+tableName+"::create(['model_column'=>$request->input('input_html'),'model_column2'=>$request->input('input_html2'),]);";
+		controllerOutput += "\n |\t\t\t//$"+tableSingular+" = new "+tableName+"; $"+tableSingular+"->name = $request->input('input_html'); $"+tableSingular+"->save(); //insertedId = $"+tableSingular+"->id;";
+		controllerOutput += "\n |\t\t\t\\Session::flash('flash_message',[\n |\t\t\t\t'msg'=>\""+tableName+" successfully stored!\", \n |\t\t\t\t'class'=>\"alert-success\"\n |\t\t\t]);";
+		controllerOutput += "\n |\t\t\treturn redirect()->route('cruds."+tableSingular+".index');";
+		controllerOutput += "\n |\t\t}else{\n |\t\t\treturn redirect()->route('cruds."+tableSingular+".index');\n |\t\t}";
+		controllerOutput += "\n |\t}";
+
+		//show
+		controllerOutput += "\n |\n |\t/** \n |\t * Display the specified resource. \n |\t * \n |\t * @param  int  $id \n |\t * @return \\Illuminate\\Http\\Response\n |\t */\n |\tpublic function show($id)\n |\t{";
+		controllerOutput += "\n |\t\tif(999==999){ // input your acl or condition";
+		controllerOutput += "\n |\t\t\t$"+tableSingular+" = \\App\\Models\\"+tableName+"::find($id);";
+		controllerOutput += "\n |\t\t\t// get previous user id";
+		controllerOutput += "\n |\t\t\t$previous = \\App\\Models\\"+tableName+"::where('"+col_id+"', '<', $"+tableSingular+"->"+col_id+")->max('"+col_id+"');";
+		controllerOutput += "\n |\t\t\tif($previous==null){";
+		controllerOutput += "\n |\t\t\t\t$previous = \\App\\Models\\"+tableName+"::orderBy('"+col_id+"','desc')->value('"+col_id+"');";
+		controllerOutput += "\n |\t\t\t}";
+		controllerOutput += "\n |\t\t\t// get next user id";
+		controllerOutput += "\n |\t\t\t$next = \\App\\Models\\"+tableName+"::where('"+col_id+"', '>', $"+tableSingular+"->"+col_id+")->min('"+col_id+"');";
+		controllerOutput += "\n |\t\t\tif($next==0){";
+		controllerOutput += "\n |\t\t\t\t$next = \\App\\Models\\"+tableName+"::orderBy('"+col_id+"','asc')->value('"+col_id+"');";
+		controllerOutput += "\n |\t\t\t}";
+		//controllerOutput += "\n |\t\t\t//return redirect()->route('cruds."+tableSingular+".show', compact('"+tableSingular+"','previous','next'));";
+		controllerOutput += "\n |\t\t\treturn view('cruds."+tableSingular+".show', compact('"+tableSingular+"','previous','next','id'));";
+		controllerOutput += "\n |\t\t}else{\n |\t\t\treturn redirect()->route('cruds."+tableSingular+".index');\n |\t\t}";
+		controllerOutput += "\n |\t}";
+		
+		//edit
+		controllerOutput += "\n |\n |\t/** \n |\t * Show the form for editing the specified resource. \n |\t * \n |\t * @param  int  $id \n |\t * @return \\Illuminate\\Http\\Response\n |\t */\n |\tpublic function edit($id)\n |\t{\n |\t";
+		controllerOutput += "\n |\t\tif(999==999){ // input your acl or condition";
+		controllerOutput += "\n |\t\t\t$"+tableSingular+" = \\App\\Models\\"+tableName+"::find($id);";
+		controllerOutput += "\n |\t\t\treturn view('cruds."+tableSingular+".edit', compact('"+tableSingular+"'));";
+		controllerOutput += "\n |\t\t}else{\n |\t\t\treturn redirect()->route('cruds."+tableSingular+".index');\n |\t\t}";
+		controllerOutput += "\n |\t}";
+		
+		//update
+		controllerOutput += "\n |\n |\t/** \n |\t * Update the specified resource in storage. \n |\t *\n |\t * @param  \\Illuminate\\Http\\Request  $request \n |\t * @param  int  $id \n |\t * @return \\Illuminate\\Http\\Response\n |\t */\n |\tpublic function update(\\App\\Http\\Requests\\"+tableName+"Request $request, $id)\n |\t{//Request $request";
+		controllerOutput += "\n |\t\tif(999==999){ // input your acl or condition";
+		controllerOutput += "\n |\t\t\t\\App\\Models\\"+tableName+"::find($id)->update($request->all());";
+		controllerOutput += "\n |\t\t\t$"+tableSingular+" = \\App\\Models\\"+tableName+"::find($id);// $"+tableSingular+"->name=Input::get('name');"+tableSingular+"->save()//$request->input('input_html')"; 
+		controllerOutput += "\n |\t\t\t\\Session::flash('flash_message',[\n |\t\t\t\t'msg'=>\""+tableName+" successfully updated!\", \n |\t\t\t\t'class'=>\"alert-success\"\n |\t\t\t]);";
+		controllerOutput += "\n |\t\t\treturn redirect()->route('cruds."+tableSingular+".index');";
+		controllerOutput += "\n |\t\t}else{\n |\t\t\treturn redirect()->route('cruds."+tableSingular+".index');\n |\t\t}";
+		controllerOutput += "\n |\t}";
+
+		//destroy
+		controllerOutput += "\n |\n |\t/** \n |\t * Remove the specified resource from storage. \n |\t *\n |\t * @param  int  $id \n |\t * @return \\Illuminate\\Http\\Response\n |\t */\n |\tpublic function destroy($id)\n |\t{";
+		controllerOutput += "\n |\t\tif(999==999){ // input your acl or condition";
+		controllerOutput += "\n |\t\t\t$"+tableSingular+" = \\App\\Models\\"+tableName+"::find($id);";
+		controllerOutput += "\n |\t\t\t$"+tableSingular+"->delete();";
+		controllerOutput += "\n |\t\t\t\Session::flash('flash_message',['\n |\t\t\t\tmsg'=>\""+tableName+" successfully removed!\", \n |\t\t\t\t'class'=>\"alert-success\"\n |\t\t\t]);";
+		controllerOutput += "\n |\t\t\treturn redirect()->route('cruds."+tableSingular+".index');";
+		controllerOutput += "\n |\t\t}else{\n |\t\t\treturn redirect()->route('cruds."+tableSingular+".index');\n |\t\t}";
+		controllerOutput += "\n |\t}";
+
+		//finalclass
+		controllerOutput += "\n |\n |}";
+		
+		var controllerOutuputJs = controllerOutput.replace(/[||]/, "or");
+		controllerOutuputJs = controllerOutuputJs.replace(/[|]/g, "");
+		controllerOutuputJs = controllerOutuputJs.replace("or", "||");
+
+		$("#controller_string_output").val(controllerOutuputJs);
+		// $("#controller_out_post").val(controllerOutput);
+
+	},
+};
+var Model = {
+	GenerateModelString:function(){
+		let tableName = $("#tableName").val();
+		let tableSingular = $("#tableSingular").val();
+		let tablePlural = $("#tablePlural").val();
+		let totalColumns = $("#totalColumns").html();
+		let html_name = "";
+		let modelString = "";
+
+	    modelString +="";
+
+	    modelString+="<\?php";
+	    modelString+=" \n | ";
+	    modelString+=" \n |namespace App\\Models;";
+	    modelString+=" \n | ";
+	    modelString+=" \n | use Illuminate\\Database\\Eloquent\\Model;";
+	    modelString+=" \n | ";
+	    modelString+=" \n | class "+tableName+" extends Model";
+	    modelString+=" \n | {";
+	    modelString+=" \n | \t\t//protected $table = 'furys';//table name";
+	    modelString+=" \n | \t\t//protected $primaryKey = 'pdt_id';//table pk";
+	    modelString+=" \n | \t\tprotected $fillable = [";
+	    modelString+=" \n | \t\t\t";
+
+	    for (var i = 0; i < totalColumns; i++) {
+    		html_name = $("#html_name"+[i]).val();
+	    		if(html_name != null){
+		        	modelString +="'"+html_name+"', ";
+	    		}else{
+	    			totalColumns++ ;
+	    		}
+	    }
+
+	    modelString+=" \n | \t\t];";
+	    
+	    modelString+=" \n | ";
+	    modelString+=" \n | \t\t//public function hasManyFunction(){";
+	    modelString+=" \n | \t\t//  return $this->hasMany('HasManyFromThis-App\\OtherModelName','OtherTableId-column_table','thisTableRelationId-id');";
+	    modelString+=" \n | \t\t//}";
+	    modelString+=" \n |";
+	    modelString+=" \n | \t\t//public function belongsToThisModelItem(){";
+	    modelString+=" \n | \t\t//return $this->belongsToMany('belongsToThis-App\\OtherModelName');";
+	    modelString+=" \n | \t\t//}";
+	    modelString+=" \n |";
+	    modelString+=" \n | \t\t//public function hasOne/singularName(){";
+	    modelString+=" \n | \t\t//return $this->hasOne('HasOneOfThis-App\\OtherModelName','OtherTableId-column_table','thisTableRelationId-id')";
+	    modelString+=" \n | \t\t//}";
+
+	    modelString+=" \n | }";
+
+	    var modelStringJs = modelString.replace(/[|]/g, "");
+
+		$("#model_string_output").val(modelStringJs);
+	}
+};
+var Request = {
+	GenerateRequestString(){
+	    let tableName = $("#tableName").val();
+	    let totalColumns = $("#totalColumns").html();
+
+	    let requestString = "";
+
+	    requestString+="<\?php";
+	    requestString+=" \n | ";
+	    requestString+=" \n | namespace App\\Http\\Requests;";
+	    requestString+=" \n | ";
+	    requestString+=" \n | use Illuminate\\Foundation\\Http\\FormRequest;";
+	    requestString+=" \n | ";
+	    requestString+=" \n | class "+tableName+"Request extends FormRequest";
+	    requestString+=" \n | {";
+
+	    requestString+=" \n | \t\t/**";
+	    requestString+=" \n | \t\t * Determine if the user is authorized to make this request.";
+	    requestString+=" \n | \t\t *";
+	    requestString+=" \n | \t\t * @return bool";
+	    requestString+=" \n | \t\t */";
+	    requestString+=" \n | \t\tpublic function authorize()";
+	    requestString+=" \n | \t\t{";
+	    requestString+=" \n | \t\t\treturn true;";
+	    requestString+=" \n | \t\t}";
+	    requestString+=" \n |";
+	    requestString+=" \n | \t\t/**";
+	    requestString+=" \n | \t\t * Get the validation rules that apply to the request.";
+	    requestString+=" \n | \t\t *";
+	    requestString+=" \n | \t\t * @return array";
+	    requestString+=" \n | \t\t */";
+	    requestString+=" \n | \t\t";
+	    requestString+=" \n | \t\tpublic function rules()";
+	    requestString+=" \n | \t\t{";
+	    requestString+=" \n | \t\t\treturn [";
+
+	    for (var i = 0; i < totalColumns; i++) {
+    		html_name = $("#html_name"+[i]).val();
+	    		if(html_name != null){
+		        	// modelString +="'"+html_name+"', ";
+		        	requestString +="\n | \t\t\t\t'"+html_name+"'=>'required|max:20',";
+	    		}else{
+	    			totalColumns++ ;
+	    		}
+	    }
+
+	    requestString+=" \n | \t\t\t];";
+	    requestString+=" \n | \t\t}";
+
+        requestString+=" \n | \t\tpublic function messages()";
+        requestString+=" \n | \t\t{";
+        requestString+=" \n | \t\t\treturn [";
+
+    	    for (var i = 0; i < totalColumns; i++) {
+        		html_name = $("#html_name"+[i]).val();
+    	    		if(html_name != null){
+    		        	// modelString +="'"+html_name+"', ";
+    		        	// requestString +="\n | \t\t\t\t'"+html_name+"'=>'required|max:20',";
+		    	        requestString +="\n | \t\t\t\t'"+html_name+".required'=>'Field required',";
+
+    	    		}else{
+    	    			totalColumns++ ;
+    	    		}
+    	    }
 
 
+        requestString+=" \n | \t\t\t];";
+        requestString+=" \n | \t\t}";
+        
+	    requestString+="\n | //rules options: max:number|min:number|email|unique:posts|bail|nullable|date";
 
-// function deleteColumn(objButton){  
-//     alert(objButton.id);
-//     let idColum= objButton.id.slice(-1); // "c";
-//     alert(idColum);
-    
+	    requestString+=" \n | }";
 
-//     $( "#"+idColum+"" ).remove();
+	   	// var requestStringJs = requestString.replace(/[|]/g, "");
+	   	var requestStringJs = requestString; 
 
-//     let newTotalColumns = $("#totalColumns").html();
-// 	newTotalColumns = parseInt(newTotalColumns) -1;
-// 	$("#totalColumns").html(newTotalColumns);
-
-
-//     Index.CreatingNewJsonFile();
-
-
-// }
+	    $("#request_string_output").val(requestStringJs);    
+	    // $("#request_out_post").val(requestString);    
+	}
+};
--- index.php
+++ index.php
@@ -12,7 +12,9 @@
 	<p><a href="http://api.jquery.com/jquery.getjson/" target="_blank">Base</a></p>
 	<p><a href="https://stackoverflow.com/questions/14060271/jquery-getjson-and-each-looping-through-child-array-not-linked-to-parent-index?rq=1" target="_blank">Looping nests</a></p>
 
-
+<!-- INDEX -->
+<hr>
+<h1>Table</h1>
 <form enctype=”multipart/form-data” method="POST" action="save-json.php" id="saveForm">
 	<!-- Numero de colunas: <label id="totalColumns"></label> -->
 	<p><span>Numero de colunas: </span><span id="totalColumns"></span><span id="lastIdSpan">(LastId:<span id="lastId"></span>)</span></p>
@@ -44,6 +46,37 @@
 
 <br>
 
+<!-- ROUTES -->
+<hr>
+<h1>Routes</h1>
+<p><button id="generateRoutes" type="button">Gerar rotas</button></p>
+<textarea id="routes_string_output"></textarea>
+
+<!-- CONTROLLER -->
+<hr>
+<h1>Controller</h1>
+<p><button id="generateController" type="button">Gerar controlador</button></p>
+<textarea id="controller_string_output"></textarea>
+
+<!-- MODEL -->
+<hr>
+<h1>Model</h1>
+<p><button id="generateModel" type="button">Gerar model</button></p>
+<textarea id="model_string_output"></textarea>
+
+<!-- MODEL -->
+<hr>
+<h1>Request</h1>
+<p><button id="generateRequest" type="button">Gerar request</button></p>
+<textarea id="request_string_output"></textarea>
+
+
+
+
+
+
+
+
 
 
 
@@ -52,3 +85,6 @@
 
 
 </html>
+
+
+
